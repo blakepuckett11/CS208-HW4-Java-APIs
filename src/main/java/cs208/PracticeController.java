aeb7a1b11fc30805a870061aa53874ec467f0d87
blakepuckett11 <143349177+blakepuckett11@users.noreply.github.com>
2023-09-26 15:36:46 -0600

    Update PracticeController.java

--- src/main/java/cs208/PracticeController.java
+++ src/main/java/cs208/PracticeController.java
@@ -1,25 +1,116 @@
 package cs208;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
 
 @RestController
 public class PracticeController
 {
-    // TODO: create a GET route with two query parameters
+    // http://localhost:8080/display_name?first_name=<yourFirstName>&last_name=<yourLastName>
+   @GetMapping("/display_name")
+    public String getTwoParameters(
+            @RequestParam("first_name") String parameter1,
+            @RequestParam("last_name") String parameter2) {
+        System.out.println("PracticeController.getTwoParameters - START");
+        System.out.println("parameter1 sent in the browser URL = " + parameter1);
+        System.out.println("parameter2 sent in the browser URL = " + parameter2);
+        System.out.println("PracticeController.getTwoParameters - END");
+        String valueReturnedToBrowser =
+                parameter1 + " " + parameter2 +"<br>";
+
+        return valueReturnedToBrowser;
+    }
 
 
-    // TODO: create a GET route with a path parameter
+    @GetMapping("/user/{username}/profile")
+    String getUsernameWithPathname(@PathVariable("username") String username)
+    {
+
+        System.out.println("Parameter received in the path of the URL:");
+        System.out.println("Username = " + username);
 
 
-    // TODO: create a POST route with two form parameters
+        String valueReturnedToBrowser =
+                        "Username = " + username + "<br>";
+
+        return valueReturnedToBrowser;
+    }
 
 
-    // TODO: create a PATCH route
+    @PostMapping("/create_user")
+    String postNewUser(
+            @RequestParam("first_name") String firstName,
+            @RequestParam("last_name") String lastName
+    )
+    {
+
+        System.out.println("Parameters received in the body of the POST request:");
+        System.out.println("title       = " + firstName);
+        System.out.println("description = " + lastName);
 
 
-    // TODO: create a DELETE route
+        String valueReturnedToClient =
+
+                        "First name = " + firstName + "\n" +
+                        "Last name = " + lastName;
 
 
-    // TODO: create a GET API that returns a random resource
+        return valueReturnedToClient;
+    }
+
+
+
+    @PatchMapping("/user/{username}/update_profile")
+    String patch(
+            @PathVariable("username") String username,
+            @RequestParam("theme") String theme,
+            @RequestParam("language_code") String code
+    )
+    {
+
+        System.out.println("Username = " + username);
+        System.out.println("Theme = " + theme);
+        System.out.println("Code    = " + code);
+
+
+        String valueReturnedToClient =
+                        "Username = " + username + "\n" +
+                        "Theme = " + theme + "\n" +
+
+                        "Code    = " + code;
+
+        return valueReturnedToClient;
+    }
+
+
+    @DeleteMapping("/resource/{resource_id}")
+    String deleteID(@PathVariable("resource_id") String resourceId,
+                  @RequestParam("authentication_token") String token)
+
+    {
+
+        System.out.println("Parameter received in the path of the URL:");
+        System.out.println("resource_id = " + resourceId);
+
+
+
+        String valueReturnedToClient =
+
+                        "resource_id = " + resourceId + " " +
+                                "has been deleted";
+
+
+        return valueReturnedToClient;
+    }
+
+
+    @GetMapping("/api/random_item")
+    String getAPI() {
+        String[] array = {"banana" , "stick", "shoe", "basketball", "water bottle"};
+        Random random = new Random();
+        int index = random.nextInt(array.length);
+        return array[index];
+}
 
 }
